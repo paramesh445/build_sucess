0b5c566828f7b68f82634149d6032f7fad439dcb
paramesh <paramesh@gmail.com>
2021-09-25 21:53:38 +0530

    all sucess .war

---
+++ src/main/java/hdfs_funds/web/views/FrontView.java
@@ -0,0 +1,10 @@
+package hdfs_funds.web.views;
+
+import net.contextfw.web.application.component.Component;
+import net.contextfw.web.application.lifecycle.PageScoped;
+import net.contextfw.web.application.lifecycle.View;
+
+@View(url="/", parent=RootView.class)
+@PageScoped
+public class FrontView extends Component {
+}
